4697a7f2b70695f4ff2ba54770a5ebe11e1bc272
Dylan Gressick <dgressick13@southhills.edu>
2017-10-10 11:55:51 -0400

    JS concept works, beginning to implement

--- sandboxDylan/js/main.js
+++ sandboxDylan/js/main.js
@@ -1,10 +1,20 @@
+var per1 = document.getElementById('period1');
+var per2 = document.getElementById('period2');
+
+
 var timer = setInterval(function() {
 
-  var time = new Date();
-  var currentHour = time.getHours();
+  var currentTime = new Date();
 
-  if (currentHour == 8) {
+  var currentHour = currentTime.getHours();
+  var currentMin = currentTime.getMinutes();
 
+  currentHour = 8;
+
+  if ((currentHour == 8) && (currentMin < 50)){
+    per1.style.backgroundColor = 'lightyellow';
+  } else if ((currentHour == 8) && (currentMin >= 50)){
+    per2.style.backgroundColor = 'lightyellow';
   }
 
 
