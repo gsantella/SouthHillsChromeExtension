03bb3866c89efda766770feaccce7614438f451a
rlaird21 <32648712+rlaird21@users.noreply.github.com>
2017-10-16 14:45:13 -0400

    finished working in the weatherAPP and got the code to work

--- sandboxRichard/weatherWork/js/main.js
+++ sandboxRichard/weatherWork/js/main.js
@@ -1,59 +1,30 @@
-// Current Temperature Elements
 var altoonaTempElement = document.getElementById('temperature_altoona');
 var stateCollegeTempElement = document.getElementById('temperature_statecollege');
 var lewistownTempElement = document.getElementById('temperature_lewistown');
+var altoona_icon = document.getElementById('weather_icon_altoona');
+var statecollege_icon = document.getElementById('weather_icon_statecollege');
+var lewistown_icon = document.getElementById('weather_icon_lewistown');
 
-// Execute AJAX call every 5 seconds
-var timer = setInterval(function() {
+
   var request = new XMLHttpRequest();
 
-  // Setup the request
-  request.open('GET', 'http://api.openweathermap.org/data/2.5/group?id=5178195,5213681,5197850&units=imperial&appid=fae79d6ccbaa879882d5706085ebe091', true);
-
-  // Called when data is back from data API
+  request.open('GET', 'http://api.openweathermap.org/data/2.5/group?id=5178195,5213681,5197850&units=imperial&appid=fae79d6ccbaa879882d5706085ebe091', true)
   request.onload = function() {
     var dataFromAPI = JSON.parse(request.responseText);
+    //get the weather
     altoonaTempElement.innerHTML = Math.round(dataFromAPI.list[0].main.temp);
     stateCollegeTempElement.innerHTML = Math.round(dataFromAPI.list[1].main.temp);
     lewistownTempElement.innerHTML = Math.round(dataFromAPI.list[2].main.temp);
+    //get the icon
+    altoona_icon.src = 'http://openweathermap.org/img/w/' + dataFromAPI.list[0].weather[0].icon + '.png';
+    statecollege_icon.src = 'http://openweathermap.org/img/w/' + dataFromAPI.list[1].weather[0].icon + '.png';
+    lewistown_icon.src = 'http://openweathermap.org/img/w/' + dataFromAPI.list[2].weather[0].icon + '.png';
     console.log(dataFromAPI);
   }
 
-  // Called whent ehre was a connection error to the data API
+  // Called when there was a connection error to the data API
   request.onerror = function() {
     console.log("Something went wrong!")
   }
 
   request.send();
-
-},  5000);
-/*
-// Current Temperature Elements
-var altoonaIcon = document.getElementById('weather_icon_altoona');
-var stateCollegeIcon = document.getElementById('weather_icon_statecollege');
-var lewistownIcon = document.getElementById('weather_icon_lewistown');
-
-// Execute AJAX call every 5 seconds
-var timer = setInterval(function() {
-  var request = new XMLHttpRequest();
-
-  // Setup the request
-  request.open('GET', 'https://openweathermap.org/weather-conditions', true);
-
-  // Called when data is back from data API
-  request.onload = function() {
-    var dataFromAPI = JSON.parse(request.responseText);
-    altoonaIcon.innerHTML = dataFromAPI.list[0].weather.icon);
-    stateCollegeIcon.innerHTML = dataFromAPI.list[1].weather.icon);
-    lewistownIcon.innerHTML = dataFromAPI.list[2].weather.icon);
-    console.log(dataFromAPI);
-  }
-
-  // Called whent ehre was a connection error to the data API
-  request.onerror = function() {
-    console.log("Something went wrong!")
-  }
-
-  request.send();
-
-},  5000);*/
