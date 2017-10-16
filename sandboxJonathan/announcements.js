2b3cebb34d8677bd086e66e30004987d67af5cc0
Jonathan Mosel <jmosel38@southhills.edu>
2017-10-16 14:46:38 -0400

    Looping through the announcements

--- sandboxJonathan/announcements.js
+++ sandboxJonathan/announcements.js
@@ -1,15 +1,40 @@
-$(function() {
+// Note: add variable length of announcements with array
+
+/*$(function() {
   $.getJSON('http://thadpi.dynu.net:8000/', function(data) {
-    //announcement 1
-    $('#ts0').html(data.announcements[0].ts);
-    $('#announ0').html(data.announcements[0].msg);
-    //announcement 2
-    $('#ts1').html(data.announcements[1].ts);
+      $.each(data.announcements, function(index) {
+          $('#ts' + index).html(moment(data.announcements[index].ts , 'YYYYMMDD').fromNow());
+          $('#announ' + index).html(data.announcements[index].msg);
+      });*/
 
-    //announcement 3
-    $('#ts2').html(data.announcements[2].ts);
+var test = ['hi1', 'hi2', 'hi3', 'hi4', 'hi5', 'hi6']
 
-    //announcement 4
-    $('#ts3').html(data.announcements[3].ts);
+$(function() {
+  $.each(test, function(index) {
+    $('ul').append('<li></li>')
+    $('li').html(test[index]);
   });
 });
+
+
+
+
+
+
+    //announcement 1
+    /*$('#ts0').html(moment(data.announcements[0].ts , 'YYYYMMDD').fromNow());
+    $('#announ0').html(data.announcements[0].msg);
+
+    //announcement 2
+    $('#ts1').html(moment(data.announcements[1].ts , 'YYYYMMDD').fromNow());
+    $('#announ1').html(data.announcements[1].msg);
+
+    //announcement 3
+    $('#ts2').html(moment(data.announcements[2].ts , 'YYYYMMDD').fromNow());
+    $('#announ2').html(data.announcements[2].msg);
+
+    //announcement 4
+    $('#ts3').html(moment(data.announcements[3].ts , 'YYYYMMDD').fromNow());
+    $('#announ3').html(data.announcements[3].msg);
+  });
+});*/
