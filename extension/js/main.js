5aa0d36c650901088cdf2d2aaa9178b37d3c6036
rlaird21 <32648712+rlaird21@users.noreply.github.com>
2017-10-17 11:38:04 -0400

    updated weather APP to have weather description in main extension

--- extension/js/main.js
+++ extension/js/main.js
@@ -69,6 +69,9 @@ day.innerHTML = weekDays[currentDay];
 var altoonaTempElement = document.getElementById('temperature_altoona');
 var stateCollegeTempElement = document.getElementById('temperature_statecollege');
 var lewistownTempElement = document.getElementById('temperature_lewistown');
+var weather_statecollege = document.getElementById('weather_statecollege')
+var weather_altoona = document.getElementById('weather_altoona')
+var weather_lewistown = document.getElementById('weather_lewistown')
 var altoona_icon = document.getElementById('weather_icon_altoona');
 var statecollege_icon = document.getElementById('weather_icon_statecollege');
 var lewistown_icon = document.getElementById('weather_icon_lewistown');
@@ -83,6 +86,10 @@ var lewistown_icon = document.getElementById('weather_icon_lewistown');
     altoonaTempElement.innerHTML = Math.round(dataFromAPI.list[0].main.temp);
     stateCollegeTempElement.innerHTML = Math.round(dataFromAPI.list[1].main.temp);
     lewistownTempElement.innerHTML = Math.round(dataFromAPI.list[2].main.temp);
+    //weather description
+    weather_statecollege.innerHTML = dataFromAPI.list[0].weather[0].description;
+    weather_altoona.innerHTML = dataFromAPI.list[1].weather[0].description;
+    weather_lewistown.innerHTML = dataFromAPI.list[2].weather[0].description;
     //get the icon
     altoona_icon.src = 'http://openweathermap.org/img/w/' + dataFromAPI.list[0].weather[0].icon + '.png';
     statecollege_icon.src = 'http://openweathermap.org/img/w/' + dataFromAPI.list[1].weather[0].icon + '.png';
